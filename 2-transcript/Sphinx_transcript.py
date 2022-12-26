e658715e08caad87332d46ca35bc06311887f395
Clement Mauget <clement.mauget@gmail.com>
2022-12-26 14:21:49 +0100

    changed sr_text in Sphinx_transcript (class)

---
+++ 2-transcript/Sphinx_transcript.py
@@ -0,0 +1,19 @@
+import speech_recognition as sr
+
+audio_file = "/home/cmauget/Bureau/Wikit/600898_short.wav"
+
+class Sphinx_transcript:
+
+    def __init__(self) -> None:
+        self.r = sr.Recognizer()
+
+    def transcript(self, audio_file):
+        with sr.AudioFile(audio_file) as source:
+            self.r.adjust_for_ambient_noise(source)
+            audio = self.r.record(source)  # read the entire audio file
+        return self.r.recognize_sphinx(audio, language="fr-FR")
+
+if __name__ == "__main__":
+    test  = Sphinx_transcript()
+    audio_file = "../1-input/600898_short.wav"
+    print(test.transcript(audio_file))
