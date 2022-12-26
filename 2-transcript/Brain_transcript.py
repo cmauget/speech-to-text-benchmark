30560382273ca29943eedb57f76c0c208e8865c9
Clement Mauget <clement.mauget@gmail.com>
2022-12-26 14:22:20 +0100

    chnaged sp_test in Brain_trascript (class)

---
+++ 2-transcript/Brain_transcript.py
@@ -0,0 +1,17 @@
+from speechbrain.pretrained import EncoderDecoderASR
+
+class Brain_transcript:
+
+    def __init__(self):
+        self.model = EncoderDecoderASR.from_hparams(source="/speechbrain/asr-crdnn-commonvoice-fr", savedir="../models/pretrained_models/asr-crdnn-commonvoice-fr")
+
+    def transcript(self, audio_file):
+        return self.model.transcribe_file(audio_file)
+
+
+
+if __name__ == "__main__":
+    test  = Brain_transcript()
+    audio_file = "../1-input/600898_short.wav"
+    print(test.transcript(audio_file))
+
