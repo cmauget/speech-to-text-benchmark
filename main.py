968b4deaa4b0d0aeb5c78d6bfc8889c243ae02d8
Nicolas <nicolas.rosalch@gmail.com>
2022-12-26 12:19:57 +0100

    Changements main +Sepa

--- main.py
+++ main.py
@@ -6,16 +6,14 @@ separateur = Separateur()
 transcripter = Kaldi_transcript()
 output = Output()
 
-document_a_traiter = sys.argv[1]
+document_a_traiter = str(sys.argv[1])
 print(document_a_traiter)
 
-nom_fichier_personne_0 = "personne0.wav"
-nom_fichier_personne_1 = "personne1.wav"
 
-audio_tab = separateur.split_sb(document_a_traiter)
-nb_personnes_entendues = separateur.export_np(audio_tab)
-
+#audio_tab = separateur.split_sb(document_a_traiter)
+#nb_personnes_entendues = separateur.export_np(audio_tab)
+nb_personnes_entendues = 2
 for i in range(nb_personnes_entendues):
     separateur.split_silence("personne{0}.wav".format(i),"p{0}".format(i))
 
-output.to_JSON(transcripter,"p0","p1","Personne0","Personne1",sys.argv[2])
+output.to_JSON(transcripter,"p0","p1","Personne0","Personne1",str(sys.argv[2]))
